13969757274f3ba9eebf581979e12918ebc5f93d
tray <onesimus.githua@student.moringaschool.com>
2023-10-07 09:18:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(nums, target) {
+  const Numbers =  new Set();
+
+  for (const num of nums) {
+      const complement = target-num;
+if (Numbers.has(complement)) {
+          return true;
+      }
+
+      Numbers.add(num);
+  }
+
+  return false;
 }
+
+// Example usage:
+const numbers = [3,9,34,8 ,56, 43];
+const targetSum = 88;
+
+const result = hasTargetSum(numbers, targetSum);
+console.log(result)
+
+
+
+
+
+  // Write your algorithm here
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,6 +32,10 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  write a function
+  create an array of numbers
+    create a target number
+    return true if any of the pairs in the array add up to the target number
 */
 
 /*
